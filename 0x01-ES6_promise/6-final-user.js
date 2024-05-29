73f8173eee836d3ddcede3cc86c77cb34799cb1a
kageruka02 <leonkageruka@gmail.com>
2024-05-29 23:49:19 +0300

    the promise returns

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,13 +1,12 @@
 import signUpUser from './4-user-promise';
-
 import uploadPhoto from './5-photo-reject';
 
-export default async function handleProfileSignup(
-  firstName,
-  lastName,
-  fileName,
-) {
-  const signUPUser = await signUpUser(firstName, lastName);
-  const fileNam = await uploadPhoto(fileName);
-  return Promise.all([signUPUser, fileNam]).then((response) => response);
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise.allSettled([
+    signUpUser(firstName, lastName),
+    uploadPhoto(fileName),
+  ]).then((responses) => responses.map((response) => ({
+    status: response.status,
+    value: response.status === 'fulfilled' ? response.value : response.reason,
+  })));
 }
