79dc4c89bb4dbf0c0e212f666950ae473d430473
kageruka02 <leonkageruka@gmail.com>
2024-05-30 19:57:27 +0300

    initializing a classs

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -1,5 +1,5 @@
 export default class classRoom {
-    constructor(maxStudentsSize) {
-        this._maxStudentsSize = maxStudentsSize
-    }
+  constructor(maxStudentsSize) {
+    this._maxStudentsSize = maxStudentsSize;
+  }
 }
