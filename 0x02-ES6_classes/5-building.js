215c8346dd7ab6fa4ca7c4505b132c7b8f477bf2
kageruka02 <leonkageruka@gmail.com>
2024-05-30 22:16:28 +0300

    use of classes to manipulate cash

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,20 @@
+export default class Building {
+  constructor(sqft) {
+    if (this.constructor !== Building) {
+      if (!Object.prototype.hasOwnProperty.call(this, 'evacuationWarningMessage')) {
+        throw new Error(
+          'Class extending Building must override evacuationWarningMessage',
+        );
+      }
+    }
+    this._sqft = sqft;
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  set sqft(value) {
+    this._sqft = value;
+  }
+}
