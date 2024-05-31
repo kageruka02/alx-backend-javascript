76d964d8592b6519c98558911d61d4d10ea3a868
kageruka02 <leonkageruka@gmail.com>
2024-05-31 23:28:25 +0300

    use of try and catch

---
+++ 0x01-ES6_promise/100-await.js
@@ -0,0 +1,17 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default async function asyncUploadUser() {
+  try {
+    const [photo, user] = await Promise.all([uploadPhoto(), createUser()]);
+    return {
+      photo,
+      user,
+    };
+  } catch (error) {
+    console.error(error);
+    return {
+      photo: null,
+      user: null,
+    };
+  }
+}
