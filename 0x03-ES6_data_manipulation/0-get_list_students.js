085bfc6bb9ae663a482b1be8b20510df0e057f46
kageruka02 <leonkageruka@gmail.com>
2024-06-03 18:28:57 +0300

    array functions

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,7 +1,10 @@
 export default function getListStudents() {
-    function createArray(firstName, id, location) {
-        return {id, firstName, location}
-    }
-    return[createArray('Guillaume',1,'San Francisco'), createArray('James',2,'Columbia'), createArray('Serena', 5, 'San Francisco')]
-    
+  function createArray(firstName, id, location) {
+    return { id, firstName, location };
+  }
+  return [
+    createArray('Guillaume', 1, 'San Francisco'),
+    createArray('James', 2, 'Columbia'),
+    createArray('Serena', 5, 'San Francisco'),
+  ];
 }
