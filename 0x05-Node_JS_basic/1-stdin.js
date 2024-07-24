09cdf828b5dfe9b9bfbde32690c4040880900b44
kageruka02 <leonkageruka@gmail.com>
2024-07-24 22:52:11 +0300

    handling stdin

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,13 @@
+function hello() {
+  console.log('Welcome to Holberton School, what is your name?');
+
+  process.stdin.on('data', (data) => {
+    const name = data.toString().trim();
+    console.log(`Your name is: ${name}`);
+    process.exit();
+  });
+}
+hello();
+process.on('exit', () => {
+  console.log('This important software is now closing');
+});
