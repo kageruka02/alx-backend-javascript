4bddd19084b14432dc1f6da1abd5d968a1cea5c1
kageruka02 <leonkageruka@gmail.com>
2024-07-26 23:24:40 +0300

    sinon

---
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -0,0 +1,15 @@
+// 3-payment.test.js
+const sinon = require('sinon');
+const Utils = require('./utils');
+const sendPaymentRequestToApi = require('./3-payment');
+
+describe('sendPaymentRequestToApi', () => {
+  it('should call Utils.calculateNumber with SUM, 100, 20', () => {
+    const spy = sinon.spy(Utils, 'calculateNumber');
+
+    sendPaymentRequestToApi(100, 20);
+
+    sinon.assert.calledWith(spy, 'SUM', 100, 20);
+    spy.restore();
+  });
+});
