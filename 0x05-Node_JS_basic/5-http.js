965c8e62f27b982cbccd3690f3612ecba3adbf34
kageruka02 <leonkageruka@gmail.com>
2024-07-28 23:26:22 +0300

    http

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -4,44 +4,49 @@ const fs = require('fs').promises;
 const app = http.createServer();
 const file = process.argv.length > 2 ? process.argv[2] : '';
 const port = 1245;
-function countStudents(filePath) {
-  return fs
-    .readFile(filePath, 'utf-8')
-    .then((data) => {
-      if (data.length === 0) throw new Error('Cannot load the database');
-      const lineArray = data
-        .split('\n')
-        .map((e) => e.trim())
-        .filter((e) => e !== '');
-      const objectLine = lineArray.slice(1).map((line) => {
-        const word = line.split(',');
-        return {
-          firstname: word[0],
-          lastname: word[1],
-          age: word[2],
-          field: word[3],
-        };
-      });
-      const fields = Array.from(new Set(objectLine.map((e) => e.field)));
-      const divisionall = [];
+const HOST = 'localhost';
 
-      divisionall.push(`Number of students: ${objectLine.length}`);
-      // eslint-disable-next-line no-plusplus
-      for (let i = 0; i < fields.length; i++) {
-        const division = objectLine
-          .filter((e) => e.field === fields[i])
-          .map((e) => e.firstname);
-        divisionall.push(
-          `Number of students in ${fields[i]}: ${
-            division.length
-          }. List: ${division.join(', ')}`,
-        );
-      }
-      return divisionall.join('\n');
-    })
-    .catch(() => {
-      throw new Error('Cannot load the database');
-    });
+function countStudents(filePath) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(filePath, 'utf-8')
+      .then((data) => {
+        if (data.length === 0) {
+          reject(new Error('Cannot load the database'));
+        }
+        const lineArray = data
+          .split('\n')
+          .map((e) => e.trim())
+          .filter((e) => e !== '');
+        const objectLine = lineArray.slice(1).map((line) => {
+          const word = line.split(',');
+          return {
+            firstname: word[0],
+            lastname: word[1],
+            age: word[2],
+            field: word[3],
+          };
+        });
+        const fields = Array.from(new Set(objectLine.map((e) => e.field)));
+        const divisionall = [];
+
+        divisionall.push(`Number of students: ${objectLine.length}`);
+        // eslint-disable-next-line no-plusplus
+        for (let i = 0; i < fields.length; i++) {
+          const division = objectLine
+            .filter((e) => e.field === fields[i])
+            .map((e) => e.firstname);
+          divisionall.push(
+            `Number of students in ${fields[i]}: ${
+              division.length
+            }. List: ${division.join(', ')}`,
+          );
+        }
+        resolve(divisionall.join('\n'));
+      })
+      .catch(() => {
+        reject(new Error('Cannot load the database'));
+      });
+  });
 }
 
 app.on('request', (req, res) => {
@@ -53,15 +58,16 @@ app.on('request', (req, res) => {
     res.statusCode = 200;
     res.write(Buffer.from(responseText));
   } else if (req.url === '/students') {
-    const responseParts = ['This is the list of our students'];
     countStudents(file)
       .then((report) => {
+        const responseParts = ['This is the list of our students'];
         responseParts.push(report);
         const responseText = responseParts.join('\n');
         res.setHeader('Content-Type', 'text/plain');
         res.setHeader('Content-Length', responseText.length);
         res.statusCode = 200;
         res.write(Buffer.from(responseText));
+        res.end();
       })
       .catch((error) => {
         const errorResponse = ['This is the list of our students'];
@@ -76,8 +82,7 @@ app.on('request', (req, res) => {
   }
 });
 
-app.listen(port, (error) => {
-  if (error) {
-    console.log('something went wrong');
-  }
+app.listen(port, HOST, () => {
+  process.stdout.write(`Server listening at -> http://${HOST}:${port}\n`);
 });
+module.exports = app;
